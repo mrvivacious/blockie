c71797ff3512b5faddc4363ca7281fd77b94b821
Vivek Bhookya <v.bhookya98@gmail.com>
2025-02-25 19:13:42 -0600

    create dataInterface.js for easier data access

--- blockies.js
+++ blockies.js
@@ -27,28 +27,6 @@ else {
   }
 }
 
-function getUserData() {
-  let userData = localStorage.getItem('blockies_data');
-  if (userData === null || userData == undefined) {
-    console.log('no user data');
-  
-    let blockies_data = {
-      blockies: []
-    };
-  
-    let dataToSave = JSON.stringify(blockies_data);
-    localStorage.setItem('blockies_data', dataToSave); //todo try-catch?
-
-    return blockies_data;
-  }
-
-  return userData;
-}
-
-function setUserData(dataToSave) {
-  localStorage.setItem('blockies_data', dataToSave);
-}
-
 function saveBlockieToUserData(blockieData) {
   // get user data
   let userData = JSON.parse(getUserData());
---
+++ dataInterface.js
@@ -0,0 +1,21 @@
+function getUserData() {
+  let userData = localStorage.getItem('blockies_data');
+  if (userData === null || userData == undefined) {
+    console.log('no user data');
+  
+    let blockies_data = {
+      blockies: []
+    };
+  
+    let dataToSave = JSON.stringify(blockies_data);
+    localStorage.setItem('blockies_data', dataToSave); //todo try-catch?
+
+    return blockies_data;
+  }
+
+  return userData;
+}
+
+function setUserData(dataToSave) {
+  localStorage.setItem('blockies_data', dataToSave);
+}
